a6069017aaf2c38e976469c11a83362e3c350484
vectorpoly42 <vectorpoly42@gmail.com>
2022-10-29 19:21:41 +0300

    доделали вывод ссылок на хобби, но уехали цифры

--- script/hobbies.js
+++ script/hobbies.js
@@ -1,5 +1,8 @@
 function anchors() {
 
-document.write("<"+ anchors.arguments[0]+"l class = \")
+document.write("<"+ anchors.arguments[0]+"l class =\"" + anchors.arguments[0] + "l\">");
 
+for (i = 1; i<anchors.arguments.length; i+=2)
+  document.write("<li><a href=\"#" + anchors.arguments[i] + "\">" + anchors.arguments[i+1] + "</a></li>");
+document.write("</" + anchors.arguments[0] + "l>");
 }
