0d56469232408e7ce14734610d5fa6097fac16e5
vectorpoly42 <vectorpoly42@gmail.com>
2022-10-29 19:42:09 +0300

    начало функции проверки + добавила поле телефон + добавила id к полям

---
+++ script/check.js
@@ -0,0 +1,8 @@
+function checkcontact() {
+  var fields = Array(document.getElementById('fio'),
+                    document.getElementById('gen1'),
+                    document.getElementById('gen2'),
+                    document.getElementById('age'),
+                    document.getElementById('message'),
+                    document.getElementById('mail'));
+}
